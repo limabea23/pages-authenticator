02f92abcb0ba28414f79b8cae7844104ce6c4d66
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-03-12 23:56:05 -0300

    commits

--- my-app/app/page.jsx
+++ my-app/app/page.jsx
@@ -1,13 +1,14 @@
+
 export default function Home() {
   return (
       <div className="container">
       <div className="login-card">
         <h1>LOGIN</h1>
+        <p>welcome back</p>
         <input type="text" id="taskInput" placeholder="Username"></input>
         <input type="text" id="taskInput" placeholder="Password"></input>
-        <input type="text" id="taskInput" placeholder="Email"></input>
         <button id="addTaskBtn">LOGIN</button>
-          <p>don't u have an account? click here :</p>
+        <p>don't u have an account? click here :</p>
       </div>
     </div>
   );
